df99077295b4e77fbfcf2f19d9443d9d7b6f8736
JarredTD <jarred.desrosiers@yahoo.com>
2023-06-29 01:47:11 -0700

    Test workflow

---
+++ test.py
@@ -0,0 +1,16 @@
+import random
+
+# Generate a random number between 1 and 10
+random_number = random.randint(1, 10)
+
+# Calculate the square of the random number
+square = random_number**2
+
+
+
+
+
+
+# Print the random number and its square
+print(f"The random number is: {random_number}")
+print(f"The square of the random number is: {square}")
